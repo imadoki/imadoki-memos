fa0d98cfabf2314fd5ff6192d3bac4a57aa774f3
imadoki <imadoki-lemon@outlook.jp>
2019-06-19 00:59:33 +0900

    change single column list

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -8,14 +8,12 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline">
+      <div>
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
+            <div className="is-parent column" key={post.id}>
               <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
+                className={`blog-list-item tile is-child box notification`}
               >
                 <header>
                   <p className="post-meta">
@@ -74,7 +72,6 @@ export default () => (
                 title
                 templateKey
                 date(formatString: "MMMM DD, YYYY")
-                featuredpost
               }
             }
           }
